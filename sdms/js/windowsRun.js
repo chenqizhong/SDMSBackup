d18d9925cb67b2ba719fe5cd6270e67d5f086530
陳祺忠 <sd0360@sdms.com.tw>
2025-07-17 14:02:59 +0800

    feat:依A12修改內容進行調整

--- sdms/js/windowsRun.js
+++ sdms/js/windowsRun.js
@@ -1,47 +1,56 @@
+const headerHeight = 50; // ← 根據浮動元件高度調整，比如 60、80、100
 $("#button01").click(function() {
+    const offset = $("#01").offset().top;
     $('html, body').animate({
-        scrollTop: $("#01").offset().top
+        scrollTop: offset - headerHeight
     }, 900);
 });
 
 $("#button02").click(function() {
+    const offset = $("#02").offset().top;
     $('html, body').animate({
-        scrollTop: $("#02").offset().top
+        scrollTop: offset - headerHeight
     }, 900);
 });
 
 $("#button03").click(function() {
+    const offset = $("#03").offset().top;
     $('html, body').animate({
-        scrollTop: $("#03").offset().top
+        scrollTop: offset - headerHeight
     }, 900);
 });
 
 $("#button04").click(function() {
+    const offset = $("#04").offset().top;
     $('html, body').animate({
-        scrollTop: $("#04").offset().top
+        scrollTop: offset - headerHeight
     }, 900);
 });
 
 $("#button05").click(function() {
+    const offset = $("#05").offset().top;
     $('html, body').animate({
-        scrollTop: $("#05").offset().top
+        scrollTop: offset - headerHeight
     }, 900);
 });
 
 $("#button06").click(function() {
+    const offset = $("#06").offset().top;
     $('html, body').animate({
-        scrollTop: $("#06").offset().top
+        scrollTop: offset - headerHeight
     }, 900);
 });
 
 $("#button07").click(function() {
+    const offset = $("#07").offset().top;
     $('html, body').animate({
-        scrollTop: $("#07").offset().top
+        scrollTop: offset - headerHeight
     }, 900);
 });
 
 $("#button08").click(function() {
+    const offset = $("#08").offset().top;
     $('html, body').animate({
-        scrollTop: $("#08").offset().top
+        scrollTop: offset - headerHeight
     }, 900);
 });
